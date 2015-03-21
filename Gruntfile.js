8e40fd7e13114203d20e0e969edfce4a9f78dfd9
Robel <robeltadesse01@gmail.com>
2015-03-21 13:37:00 -0400

    add gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'index.js', 'lib/*.js', 'test/*.js']
+    },
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
     }
   });
+    // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('test', 'mochaTest');
+  // for the jshint
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', ['jshint']);
-
-  grunt.loadNpmTasks('grunt-coveralls');
-  grunt.registerTask('coverage', ['coveralls']);
+  grunt.registerTask('default', ['jshint', 'mochaTest' ]);
 };
