ccf58c95c26b38cf9697e3968e572b058a014627
Robel <robeltadesse01@gmail.com>
2015-03-21 11:17:55 -0400

    added grunt coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,25 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'index.js', 'lib/*.js', 'test/*.js']
+    },
+    coveralls: {
+      options: {
+        // LCOV coverage file relevant to every target
+        src: 'coverage-results/lcov.info',
+
+        // When true, grunt-coveralls will only print a warning rather than
+        // an error, to prevent CI builds from failing unnecessarily (e.g. if
+        // coveralls.io is down). Optional, defaults to false.
+        force: false
+      },
+      your_target: {
+      // Target-specific LCOV coverage file
+        src: 'coverage-results/extra-results-*.info'
+      },
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('default', ['jshint']);
+
+  grunt.loadNpmTasks('grunt-coveralls');
 };
